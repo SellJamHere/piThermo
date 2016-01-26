0f7230131ad748b8bfa8fbd6ab1088b5499593ab
James Heller <jheller@veritone.com>
2016-01-26 00:53:08 -0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/SellJamHere/piBot/thermo"
+	"github.com/SellJamHere/thermoBot/models"
+	r "github.com/dancannon/gorethink"
+)
+
+const (
+	piSerial   = ""
+	piDeviceId = "e0a701e4-f37c-4d6b-adbb-4cc6995a266b"
+)
+
+func main() {
+	var session *r.Session
+
+	tempReader, err := thermo.NewTemperatureReader(piSerial)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	session, err = r.Connect(r.ConnectOpts{
+		Address: "162.243.156.65:28015",
+	})
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	for {
+		temp, err := tempReader.ReadTemp()
+		if err != nil {
+			fmt.Println(err)
+			panic("Error reading temp")
+		}
+
+		dbTemp := models.DbTemperatureFromThermo(*temp)
+
+		_, err = r.DB("dataWhereHouse").Table("event").Insert(dbTemp).RunWrite(session)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		time.Sleep(5 * time.Minute)
+	}
+}
---
+++ models/temperature.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"github.com/SellJamHere/piBot/thermo"
+)
+
+type DbTemperature struct {
+	Celsius      *float64   `gorethink:"celsius,omitempty"`
+	Fahrenheit   *float64   `gorethink:"fahrenheit,omitempty"`
+	MeasuredTime *time.Time `gorethink:"measuredTime,omitempty"`
+}
+
+func DbTemperatureFromThermo(temp thermo.Temperature) DbTemperature {
+	return DbTemperature{
+		Celsius:      &temp.Celsius,
+		Fahrenheit:   &temp.Fahrenheit,
+		MeasuredTime: &temp.MeasuredTime,
+	}
+}
